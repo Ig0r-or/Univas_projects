a011d72e72c0f6b66bd7585a33ef0eb011644993
Igor <164401980+Ig0r-or@users.noreply.github.com>
2024-09-25 15:10:21 -0300

    Add files via upload

---
+++ JavaScript/Relógio/script.js
@@ -0,0 +1,22 @@
+function loading(){
+
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('img')
+var data = new Date()
+var hora = data.getHours()
+
+    if(hora >= 0 && hora < 12){
+        msg.innerHTML = `Agora são ${hora} horas da manhã.`
+        img.scr = 'Imagens/Manhã.png'
+        document.body.style.background = '#87CEFA'
+    }else if(hora >= 12 && hora < 18){
+        msg.innerHTML = `Agora são ${hora} horas da tarde.`
+        img.src = 'Imagens/Tarde.png'
+        document.body.style.background = '#FFDEAD'
+    }else{
+        msg.innerHTML = `Agora são ${hora} horas da noite.`
+        img.src = 'Imagens/Noite.png'
+        document.body.style.background = '#191970'
+    }
+    
+}
