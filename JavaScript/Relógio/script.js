e6494c41ee24ccbeabf29d5d7b31e0fea238082b
Igor <164401980+Ig0r-or@users.noreply.github.com>
2024-09-27 09:38:10 -0300

    Update script.js

--- JavaScript/Relógio/script.js
+++ JavaScript/Relógio/script.js
@@ -1,22 +1,34 @@
 function loading(){
-
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('img')
-var data = new Date()
-var hora = data.getHours()
-
-    if(hora >= 0 && hora < 12){
-        msg.innerHTML = `Agora são ${hora} horas da manhã.`
+function formatTime(number)
+ {
+    return number < 10 ? '0' + number : number;
+  }
+  function updateTime() {
+  let now = new Date();
+  let hours = formatTime(now.getHours());
+  let minutes = formatTime(now.getMinutes());
+  let seconds = formatTime(now.getSeconds());  
+    if(hours >= 0 && hours < 12){
+        msg.innerHTML = `${hours}:${minutes}:${seconds}`
         img.scr = 'Imagens/Manhã.png'
         document.body.style.background = '#87CEFA'
-    }else if(hora >= 12 && hora < 18){
-        msg.innerHTML = `Agora são ${hora} horas da tarde.`
+    }else if(hours >= 12 && hours < 18){
+        msg.innerHTML = `${hours}:${minutes}:${seconds}`
         img.src = 'Imagens/Tarde.png'
         document.body.style.background = '#FFDEAD'
-    }else{
-        msg.innerHTML = `Agora são ${hora} horas da noite.`
+    }else if (hora > 12 && hora <= 24){
+        msg.innerHTML = `${hours}:${minutes}:${seconds}`
         img.src = 'Imagens/Noite.png'
         document.body.style.background = '#191970'
+    }else{
+        msg.innerHTML = `Horário inválido!!`
+        img.src = 'Imagens/error.png'
+        msg.style.color = 'red'
+        document.body.style.background = 'gray'
     }
-    
+  }
+updateTime();
+setInterval(updateTime, 1000);
 }
